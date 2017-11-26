c78156b35d95742f514a397fc2da495c8752c83b
jasonsignolet <jasonsignolet@gmail.com>
2017-11-26 16:26:00 +1030

    Add package documentation

---
+++ R/package.R
@@ -0,0 +1,20 @@
+#' European professional football data from Kaggle
+#'
+#' European professional football data from Kaggle for the
+#'   R programming course. All data taken from
+#'   https://www.kaggle.com/hugomathien/soccer
+#'
+#' @section Datasets:
+#' \describe{
+#'   \item{country}{}
+#'   \item{football_match}{}
+#'   \item{league}{}
+#'   \item{player}{}
+#'   \item{player_attributes}{}
+#'   \item{team}{}
+#'   \item{team_attributes}{}
+#' }
+#'
+#' @docType package
+#' @name football
+NULL
